0ed60c8f08b43cfee613355f698c18c9895168b3
Vinícius Junqueira <43252803+vbbjandrade@users.noreply.github.com>
2019-12-10 17:44:47 -0300

    drag

--- GamificacaoTech/public/js/objdrag.js
+++ GamificacaoTech/public/js/objdrag.js
@@ -8,6 +8,13 @@ $(function () {
     var eldrag = null;
     var eventName = "";
     var initialCellX = 0, initialCellY = 0;
+    var moveisFixos = [
+        { cellX: 2, cellY: 5, cellW: 3, cellH: 3 },
+        { cellX: 2, cellY: 0, cellW: 4, cellH: 3 },
+        { cellX: 7, cellY: 5, cellW: 4, cellH: 3 },
+        { cellX: 9, cellY: 3, cellW: 2, cellH: 2 },
+        { cellX: 12, cellY: 2, cellW: 3, cellH: 2 }
+    ];
     var contentWrapper = document.getElementById("content-wrapper");
 
     function cancelEvent(evt) {
@@ -57,10 +64,12 @@ $(function () {
 
     function mouseup() {
         isdown = false;
+
         if (eldrag) {
-            var i, item, items = $("#content-wrapper .room-object"), overlaps = false;
+            var i, item, items = moveisFixos.slice(), overlaps = false;
             var el = eldrag.cellX, et = eldrag.cellY;
             var er = el + eldrag.cellW, eb = et + eldrag.cellH;
+            Array.prototype.push.apply(items, $("#content-wrapper .room-object"));
             for (i = items.length - 1; i >= 0; i--) {
                 item = items[i];
                 if (item === eldrag)
@@ -104,7 +113,7 @@ $(function () {
             return cancelEvent(e);
         }, { capture: true, passive: false });
     
-        contentWrapper.addEventListener("touchend", mouseup, { capture: true, passive: false });
+        document.addEventListener("touchend", mouseup, { capture: true, passive: false });
     }
 
     contentWrapper.addEventListener("mousedown", function (e) {
@@ -127,5 +136,5 @@ $(function () {
         return cancelEvent(e);
     }, true);
 
-    contentWrapper.addEventListener("mouseup", mouseup, true);
+    document.addEventListener("mouseup", mouseup, true);
 });
